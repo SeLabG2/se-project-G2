6d4724db1c01918c7602f581ff6983ac2182f8b5
Drag0Learn <drago.learn.1@gmail.com>
2022-03-30 05:07:37 +0530

    can now delete posts and update stats

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import Comments from './Comments/Comments';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectAllPosts } from '../features/posts/postSlice';
+import { getDocRefById } from '../firebase/firebase-firestore';
+import { deleteDoc, increment, updateDoc } from 'firebase/firestore';
 
 function PostDetails() {
-    const { p_id } = useParams();
+    const { c_id, p_id } = useParams();
     const allPosts = useSelector(selectAllPosts);
     const post = allPosts.filter(p => p.p_id === p_id)[0];
+    const navigate = useNavigate();
 
     const handleDelete = () => {
-
+        const isOkayToDelete = window.confirm('Are you sure you want to delete?\n',
+            'All the comments will also be gone.');
+        if (isOkayToDelete) {
+            console.log('post is : ', post);
+            const totalDeletedContributions = post.total_comments + 1;
+            const postDocRef = getDocRefById(p_id, `classes/${c_id}/posts`);
+            deleteDoc(postDocRef)
+                .then(() => {
+                    const classDocRef = getDocRefById(c_id, 'classes');
+                    updateDoc(classDocRef, {
+                        total_deleted_contributions: increment(totalDeletedContributions)
+                    })
+                        .then(navigate('/'))
+                })
+                .catch(err => console.log(err.message))
+        }
     };
 
     return (
