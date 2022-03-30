6483723e085c61abb926ca5e51b43a5b753b1bd2
Drag0Learn <drago.learn.1@gmail.com>
2022-03-30 06:01:50 +0530

    statistics tab done. deleting class won't delete storage resources, will try to fix it in future

--- src/Pages/Dashboard/MainContent/ManageGeneralSettings.js
+++ src/Pages/Dashboard/MainContent/ManageGeneralSettings.js
@@ -5,6 +5,8 @@ import styled from 'styled-components';
 import { getDocRefById } from '../../../firebase/firebase-firestore';
 import { deleteDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
+import { storage } from '../../../firebase/firebase-config';
+import { deleteObject, ref } from 'firebase/storage';
 
 function ManageGeneralSettings() {
     const navigate = useNavigate();
@@ -14,10 +16,17 @@ function ManageGeneralSettings() {
         const isOkayToDelete = window.confirm("Are you sure you want to delete the class?\n",
             "You and all the joined users will lose all the data for this class.");
         if (isOkayToDelete) {
-            const classDocRef = getDocRefById(currentClass?.c_id, 'classes');
+            const class_id = currentClass?.c_id;
+            const classDocRef = getDocRefById(class_id, 'classes');
             deleteDoc(classDocRef)
                 .then(() => {
-                    navigate('/');
+                    const resourcesRef = ref(storage, `/resources/${class_id}`);
+                    deleteObject(resourcesRef)
+                        .then(() => {
+                            // Class Resources deleted successfully
+                            console.log('class deleted.');
+                            navigate('/');
+                        })
                 })
                 .catch(err => console.log(err.message))
         }
--- src/Pages/Dashboard/MainContent/StatisticsTab.js
+++ src/Pages/Dashboard/MainContent/StatisticsTab.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import { getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectCurrentClass } from '../../../features/classes/classSlice';
+import { selectUser } from '../../../features/user/userSlice';
+import { getColRef } from '../../../firebase/firebase-firestore';
+import { StyledPostCard } from '../../../components/styled/PostList.styled';
 
 function StatisticsTab() {
+    const user = useSelector(selectUser);
+    const currentClass = useSelector(selectCurrentClass);
+    const [individualStatsList, setIndividualStatsList] = useState([]);
+    const myStats = individualStatsList.filter(stat => stat.user === user.email);
+    const isOnInstructorList = currentClass?.instructors_list?.includes(user.email);
+
+    useEffect(() => {
+        const individualStatsColRef = getColRef(`classes/${currentClass.c_id}/individual_stats`);
+        getDocs(individualStatsColRef)
+            .then((snapshot) => {
+                const promises = snapshot.docs.map(doc => {
+                    return { ...doc.data(), id: doc.id }
+                });
+                Promise.all(promises)
+                    .then(stats => {
+                        setIndividualStatsList([...stats]);
+                    })
+
+            })
+            .catch(err => console.log(err.message))
+    }, [])
+
     return (
-        <div>StatisticsTab</div>
+        <div>StatisticsTab
+            <br />
+            <div>Overall Class Statistics</div>
+            {`total posts : ${currentClass.total_posts}\n`}
+            {`total contributions : ${currentClass.total_contributions}\n`}
+            {`total anonymous contributions : ${currentClass.total_anonymous_contributions}\n`}
+            {`total deleted contributions : ${currentClass.total_deleted_contributions}\n`}
+            <br />
+            <div>My Stats</div>
+            <pre>{JSON.stringify(myStats[0], null, 2)}</pre>
+            <br />
+            {
+                isOnInstructorList
+                &&
+                <>
+                    <div>Individual Stats of Students/Instructors enrolled in this class</div>
+                    {individualStatsList.map(stat => (
+                        <StyledPostCard key={stat.id}>
+                            {`${stat.user} :\n`}
+                            Total Posts : {stat.total_posts}
+                            <br />
+                            Total Contributions : {stat.total_contributions}
+                        </StyledPostCard>
+                    ))}
+                </>
+            }
+        </div>
     );
 }
 
