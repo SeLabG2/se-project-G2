003896c8f270d1178bdc3f2a51f1c8d030a92c78
Drag0Learn <drago.learn.1@gmail.com>
2022-03-30 21:04:34 +0530

    can delete class and all resources now

--- src/Pages/Dashboard/MainContent/ManageGeneralSettings.js
+++ src/Pages/Dashboard/MainContent/ManageGeneralSettings.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectCurrentClass } from '../../../features/classes/classSlice';
 import styled from 'styled-components';
-import { getDocRefById } from '../../../firebase/firebase-firestore';
-import { deleteDoc } from 'firebase/firestore';
+import { getColRef, getDocRefById } from '../../../firebase/firebase-firestore';
+import { deleteDoc, getDocs } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 import { storage } from '../../../firebase/firebase-config';
 import { deleteObject, ref } from 'firebase/storage';
@@ -18,6 +18,29 @@ function ManageGeneralSettings() {
         if (isOkayToDelete) {
             const class_id = currentClass?.c_id;
             const classDocRef = getDocRefById(class_id, 'classes');
+            const resourceColRef = getColRef(`classes/${class_id}/resources`);
+            const deleteClass = async () => {
+                try {
+                    const resourceSnapshot = await getDocs(resourceColRef);
+                    const resourcePaths = resourceSnapshot.docs.map(resource => {
+                        return `/resources/${class_id}/${resource.data().name}`
+                    })
+                    // now delete the class
+                    await deleteDoc(classDocRef);
+
+                    // now delete all the resources from the storage bucket
+                    const promises = resourcePaths.map(async (path) => {
+                        const resourceRef = ref(storage, path);
+                        await deleteObject(resourceRef);
+                    })
+                    await Promise.all(promises);
+                    console.log('class deleted.');
+                    navigate('/');
+                } catch (err) {
+                    console.log(err.message);
+                }
+            };
+            deleteClass();
             deleteDoc(classDocRef)
                 .then(() => {
                     const resourcesRef = ref(storage, `/resources/${class_id}`);
